b5d4d879100cad4c85e5df2e19f06b9ba4d8384b
Gregdevfree <gregory.goix@gmail.com>
2024-11-21 17:17:04 +0100

    Etape 2 projet 8 - eventListener sur chacune des flèches-2ème version

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,26 @@
 # Print-it-JS-main
+1er commit "Etape 1 projet 8 - ajout des flèches slider"
+-->Ajout des 2 flèches dans index.html et sans modifier le css
+
+<div id="banner"> <!--carrousel de 4 images-->
+		<img class="banner-img" src="./assets/images/slideshow/slide1.jpg" alt="Banner Print-it">
+		<!--intégration des flèches de navigation-->
+		<img class =" arrow arrow_left" src="./assets/images/arrow_left.png" alt="arrow_left"><!--voir si on renomme alt en suivant ou précédent pour accessibilité-->
+		<img class =" arrow arrow_right" src="./assets/images/arrow_right.png" alt="arrow_right">
+		<p>Impressions tous formats <span>en boutique et en ligne</span></p>
+--> suppression du doublon body {} dans style.css
+
+2e commit "Etape 2 projet 8 - eventListener sur chacune des flèches"
+-->modification du html:
+Pour améliorer l'accessibilité, ajout des attributs aria-label sur les flèches gauche et droite
+
+<div id="banner"> <!--carrousel/slider-->
+		<img class="banner-img" src="./assets/images/slideshow/slide1.jpg" alt="Banner Print-it">
+		<!--intégration des flèches de navigation-->
+		<img class="arrow arrow_left" src="./assets/images/arrow_left.png" alt="Précédente" aria-label="Image précédente">
+		<img class="arrow arrow_right" src="./assets/images/arrow_right.png" alt="Suivante" aria-label="Image suivante">
+		<p>Impressions tous formats <span>en boutique et en ligne</span></p>
+
+-->suppression de <script src="script.js" defer></script> car déjà présence de <script src="./assets/script.js"></script> en fin du <body>
+
+-->tests console.log et alert ()
--- assets/script.js
+++ assets/script.js
@@ -25,42 +25,19 @@ let arrowRight = document.querySelector('.arrow_right')
 let bannerImage = document.querySelector('.banner-img')
 let tagLine = document.querySelector('#banner p span')
 
-// Index de l'image actuellement affichée
-let currentIndex = 0
 
-// Fonction pour mettre à jour l'image et le texte du carrousel
-function updateCarousel() {
-  bannerImage.src = `./assets/images/slideshow/${slides[currentIndex].image}`
-  tagLine.innerHTML = slides[currentIndex].tagLine
-}
-
-function navigateCarousel(direction) {
-  // Si direction est 'left', on décrémente l'index, sinon on l'incrémente
-  currentIndex =
-    direction === 'left'
-      ? currentIndex === 0
-        ? slides.length - 1
-        : currentIndex - 1
-      : currentIndex === slides.length - 1
-      ? 0
-      : currentIndex + 1
-
-  // Mettre à jour l'image et le texte
-  updateCarousel()
-
-  // Test avec console.log - A supprimer à l'étape finale
+// Ajout de l'eventListener sur la flèche gauche (image précédente)
+arrowLeft.addEventListener('click', () => {
+  // Test avec console.log ou une alerte pour tester - A supprimer à l'étape finale
   console.log(
-    `Flèche ${direction} cliquée, affichage de l'image à l'index ${currentIndex}`
+    "Flèche gauche cliquée"
   )
-}
+  alert('Flèche gauche cliquée')
+})
 
-// Ajout des eventListeners
-if (arrowLeft && arrowRight && bannerImage && tagLine) {
-  arrowLeft.addEventListener('click', () => navigateCarousel('left'))
-  arrowRight.addEventListener('click', () => navigateCarousel('right'))
-} else {
-  console.error('Certains éléments DOM nécessaires sont manquants.')
-}
-
-// Initialiser le carrousel avec la première image et texte
-updateCarousel()
+// Ajout de l'eventListener sur la flèche droite (image suivante)
+arrowRight.addEventListener('click', () => {
+  // Test avec console.log ou une alerte pour tester - A supprimer à l'étape finale
+  console.log('Flèche droite cliquée')
+  alert('Flèche droite cliquée')
+})
